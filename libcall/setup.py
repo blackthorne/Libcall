816fa3c8e978ae48f2308d29f68850d75d4131cc
Francisco Ribeiro <francisco@ironik.org>
2016-12-05 00:37:50 +0000

    added setup.py

---
+++ libcall/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(name='libcall',
+      version='0.1',
+      description='Libcall',
+      author='Francisco Ribeiro',
+      author_email='blackthorne@ironik.org',
+      url='https://github.com/blackthorne/Libcall',
+      packages=['call'],
+     )
